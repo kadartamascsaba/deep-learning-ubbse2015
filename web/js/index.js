4ab288641ebfdcf8cf0385421f7b96c2896cddb6
Benedek Zoltan <ben.zoltan@yahoo.com>
2015-12-02 20:40:46 +0200

    #022, #020, #014, #011 added landing page with popup menu and social login + nodeJS server + mongodb

--- web/js/index.js
+++ web/js/index.js
@@ -1,43 +1,7 @@
-angular.module('myApp', ['ngFacebook'])
-  .config(['$facebookProvider', function($facebookProvider) {
-    $facebookProvider.setAppId('1000732483317996').setPermissions(['email','user_friends']);
-  }])
-  .run(['$rootScope', '$window', function($rootScope, $window) {
-	(function(d, s, id) {
-		var js, fjs = d.getElementsByTagName(s)[0];
-		if (d.getElementById(id)) return;
-		js = d.createElement(s); js.id = id;
-		js.src = "//connect.facebook.net/hu_HU/sdk.js#xfbml=1&version=v2.5&appId=1000732483317996";
-		fjs.parentNode.insertBefore(js, fjs);
-	}(document, 'script', 'facebook-jssdk'));
-    $rootScope.$on('fb.load', function() {
-      $window.dispatchEvent(new Event('fb.load'));
-    });
-  }])
-  .controller('myCtrl', ['$scope', '$facebook', function($scope, $facebook) {
-	  $scope.$on('fb.auth.authResponseChange', function() {
-		  $scope.status = $facebook.isConnected();
-		  if($scope.status) {
-			FB.api('/me', function(response) {
-			   alert('Good to see you, ' + response.name + '.');
-			   $scope.user = response;
-			});
-		  }
-      });
+var app = angular.module('myApp', ['ngDialog']);
 
-    $scope.loginToggle = function() {
-      if($scope.status) {
-        $facebook.logout();
-      } else {
-        $facebook.login();
-      }
+app.controller('myCtrl', function ($scope, ngDialog) {
+    $scope.clickToOpen = function () {
+        ngDialog.open({ template: 'popupTmpl.html' });
     };
-
-    $scope.getFriends = function() {
-      if(!$scope.status) return;
-      $facebook.cachedApi('/me/friends').then(function(friends) {
-        $scope.friends = friends.data;
-      });
-    }
-  }])
-;
+});
