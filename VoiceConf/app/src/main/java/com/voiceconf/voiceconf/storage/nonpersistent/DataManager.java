855bc454285a5ac3533683e050843139137c6960
Blenesi Attila <ablenesi@gmail.com>
2015-12-28 14:26:19 +0200

    Created the Data manager class

---
+++ VoiceConf/app/src/main/java/com/voiceconf/voiceconf/storage/nonpersistent/DataManager.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright 2015 Halcyon Mobile
+ * http://www.halcyonmobile.com
+ * All rights reserved.
+ */
+package com.voiceconf.voiceconf.storage.nonpersistent;
+
+import com.voiceconf.voiceconf.storage.models.Friend;
+
+import java.util.List;
+import java.util.Observable;
+
+/**
+ * Use this class to manage data for the application lifecycle;
+ *
+ * Created by Attila Blenesi on 28 Dec 2015
+ */
+public class DataManager extends Observable {
+
+    private static final int FRIENDS_UPDATED = 1; // Data type for observer notification
+
+    private List<Friend> mFriends;
+
+    public List<Friend> getFriends() {
+        return mFriends;
+    }
+
+    public void setFriends(List<Friend> friends) {
+        this.mFriends = friends;
+        setChanged();
+        notifyObservers(FRIENDS_UPDATED);
+    }
+}
