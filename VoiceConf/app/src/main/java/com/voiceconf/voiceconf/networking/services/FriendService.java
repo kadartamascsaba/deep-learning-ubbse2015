7f44b75c1063f1b234ff11b95861824262e0240a
Blenesi Attila <ablenesi@gmail.com>
2015-12-31 01:13:36 +0200

    Update get to only return the current users invites

--- VoiceConf/app/src/main/java/com/voiceconf/voiceconf/networking/services/FriendService.java
+++ VoiceConf/app/src/main/java/com/voiceconf/voiceconf/networking/services/FriendService.java
@@ -8,7 +8,6 @@ import android.widget.Toast;
 import com.parse.FindCallback;
 import com.parse.GetCallback;
 import com.parse.ParseException;
-import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
@@ -16,6 +15,7 @@ import com.voiceconf.voiceconf.storage.models.Friend;
 import com.voiceconf.voiceconf.storage.models.User;
 import com.voiceconf.voiceconf.storage.nonpersistent.VoiceConfApplication;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -25,28 +25,39 @@ public class FriendService {
 
     /**
      * Use this method to get all friends from Parse
+     *
      * @param callback If null the data will be updated in the DataManager else it will be returned,
      *                 to the callback, and if the query failed the exception will be passed to the
      *                 onFailure method.
      */
-    public static void getFriends( @Nullable final ParseGetCallback<Friend> callback){
+    public static void getFriends(@Nullable final ParseGetCallback<Friend> callback) {
         // Preparing query
-        ParseQuery<Friend> friendQuery = ParseQuery.getQuery(Friend.class);
-        friendQuery.include(Friend.FRIEND_ID)
-                .include(Friend.USER_ID);
+        ParseQuery<Friend> fromCurrent = ParseQuery.getQuery(Friend.class);
+        fromCurrent.whereEqualTo(Friend.USER_ID, ParseUser.createWithoutData(ParseUser.class, ParseUser.getCurrentUser().getObjectId()));
+
+        ParseQuery<Friend> fromOther = ParseQuery.getQuery(Friend.class);
+        fromOther.whereEqualTo(Friend.FRIEND_ID, ParseUser.createWithoutData(ParseUser.class, ParseUser.getCurrentUser().getObjectId()));
+
+        List<ParseQuery<Friend>> queries = new ArrayList<>();
+        queries.add(fromCurrent);
+        queries.add(fromOther);
+
+        ParseQuery<Friend> friendQuery = ParseQuery.or(queries);
+        friendQuery.include(Friend.FRIEND_ID);
+        friendQuery.include(Friend.USER_ID);
 
         // Running query in background
         friendQuery.findInBackground(new FindCallback<Friend>() {
             @Override
             public void done(List<Friend> objects, ParseException e) {
-                if(e == null){
-                    if(callback == null){
+                if (e == null) {
+                    if (callback == null) {
                         VoiceConfApplication.sDataManager.setFriends(objects);
-                    }else{
+                    } else {
                         callback.onResult(objects);
                     }
-                }else{
-                    if(callback!=null){
+                } else {
+                    if (callback != null) {
                         callback.onFailure(e);
                     }
                 }
@@ -56,11 +67,12 @@ public class FriendService {
 
     /**
      * User this method to add a new friend via email.
+     *
      * @param context Needed for toast to notify the user.
-     * @param email The email address for the friend
+     * @param email   The email address for the friend
      */
     public static void addNewFriend(@NonNull final Context context, @NonNull String email) {
-        if(email.equals(ParseUser.getCurrentUser().getEmail())){
+        if (email.equals(ParseUser.getCurrentUser().getEmail())) {
             Toast.makeText(context, "You cant add yourself as friend", Toast.LENGTH_LONG).show();
         }
         final Friend friend = new Friend();
