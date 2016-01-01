8c143d758314cec4f48aa14859391e9d6034077f
Blenesi Attila <ablenesi@gmail.com>
2016-01-01 23:12:14 +0200

    Added save button to settings

--- VoiceConf/app/src/main/java/com/voiceconf/voiceconf/ui/main/SettingsActivity.java
+++ VoiceConf/app/src/main/java/com/voiceconf/voiceconf/ui/main/SettingsActivity.java
@@ -1,12 +1,20 @@
 package com.voiceconf.voiceconf.ui.main;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.voiceconf.voiceconf.R;
 
+/**
+ * Created by Tamas-Csaba Kadar on 01 Jan 2016
+ * Edited by Attila Blenesi
+ */
 public class SettingsActivity extends AppCompatActivity {
 
     @Override
@@ -25,4 +33,30 @@ public class SettingsActivity extends AppCompatActivity {
             actionBar.setTitle(getString(R.string.settings));
         }
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.settings, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        switch (item.getItemId()) {
+            case R.id.action_save:
+                // TODO CR: [Tamas  | High] Add saving logic here [BAttila]
+                return true;
+            case android.R.id.home:
+                NavUtils.navigateUpTo(this, new Intent(this, MainActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
