085e9ac7d931b48ca6c76157ac3e2fc8afc3c2e1
Kádár Tamás-Csaba <triqla@gmail.com>
2016-01-01 22:39:42 +0200

    Added SharedPreferences to Settings

--- VoiceConf/app/src/main/java/com/voiceconf/voiceconf/ui/main/SettingsActivity.java
+++ VoiceConf/app/src/main/java/com/voiceconf/voiceconf/ui/main/SettingsActivity.java
@@ -1,6 +1,8 @@
 package com.voiceconf.voiceconf.ui.main;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBar;
@@ -8,6 +10,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 import com.voiceconf.voiceconf.R;
 
@@ -17,6 +20,11 @@ import com.voiceconf.voiceconf.R;
  */
 public class SettingsActivity extends AppCompatActivity {
 
+    private EditText mHostname;
+    private EditText mPort;
+
+    private SharedPreferences sharedpreferences;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +33,11 @@ public class SettingsActivity extends AppCompatActivity {
         // Toolbar setup
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        mHostname = (EditText)findViewById(R.id.set_hostname_input);
+        mPort = (EditText)findViewById(R.id.set_port_input);
+
+        initServerData();
 
         // Show the Up button in the action bar.
         ActionBar actionBar = getSupportActionBar();
@@ -50,6 +63,7 @@ public class SettingsActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             case R.id.action_save:
                 // TODO CR: [Tamas  | High] Add saving logic here [BAttila]
+                saveServerData(mHostname.getText().toString(), mPort.getText().toString());
                 return true;
             case android.R.id.home:
                 NavUtils.navigateUpTo(this, new Intent(this, MainActivity.class));
@@ -57,6 +71,22 @@ public class SettingsActivity extends AppCompatActivity {
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    private void initServerData() {
+        sharedpreferences = getSharedPreferences("VoiceConfData", Context.MODE_PRIVATE);
+        mHostname.setText(sharedpreferences.getString("hostname", ""));
+        Integer port = sharedpreferences.getInt("port", 0);
+        if (!port.equals(0)) {
+            mPort.setText(port.toString());
+        }
+    }
+
+    private void saveServerData(String hostname, String port) {
+        SharedPreferences.Editor editor = sharedpreferences.edit();
+        editor.putString("hostname", hostname);
+        editor.putInt("port", Integer.parseInt(port));
+        editor.commit();
     }
 
 }
