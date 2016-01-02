25fefee72e389f1ce8714f8b3d2ec602d8ad92b6
Blenesi Attila <ablenesi@gmail.com>
2016-01-02 22:47:50 +0200

    Bump version to v0.1.0.3

--- VoiceConf/app/src/main/java/com/voiceconf/voiceconf/ui/conference/ConferenceActivity.java
+++ VoiceConf/app/src/main/java/com/voiceconf/voiceconf/ui/conference/ConferenceActivity.java
@@ -115,10 +115,23 @@ public class ConferenceActivity extends AppCompatActivity {
             mSpeakerDuration.setText("Duration\n" + minutes / 60 + " h " + minutes % 60 + " min");
 
             mAdapter.update(mConference.getInvitees());
+
+            if(mConference.isClosed()){
+                ((TextView) findViewById(R.id.speaker_title)).setText(R.string.owner);
+
+                findViewById(R.id.conference_toolbar).setVisibility(View.GONE);
+                findViewById(R.id.conference_close).setVisibility(View.GONE);
+                findViewById(R.id.fab).setVisibility(View.GONE);
+
+                Glide.with(this).load(User.getAvatar(mConference.getOwner())).into(mSpeakerAvatar);
+                mSpeakerName.setText(mConference.getOwner().getUsername());
+
+            }else{
+                ParseUser user = ParseUser.getCurrentUser();
+                Glide.with(this).load(User.getAvatar(user)).into(mSpeakerAvatar);
+                mSpeakerName.setText(user.getUsername());
+            }
         }
-        ParseUser user = ParseUser.getCurrentUser();
-        Glide.with(this).load(User.getAvatar(user)).into(mSpeakerAvatar);
-        mSpeakerName.setText(user.getUsername());
     }
 
     @Override
