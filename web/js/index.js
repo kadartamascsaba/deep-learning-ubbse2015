2df435977b726edb46962513a12b9ff23f29ab18
Blenesi Attila <ablenesi@gmail.com>
2016-01-03 20:22:53 +0200

    Removed web related code

--- web/js/index.js
+++
@@ -1,7 +0,0 @@
-var app = angular.module('myApp', ['ngDialog']);
-
-app.controller('myCtrl', function ($scope, ngDialog) {
-    $scope.clickToOpen = function () {
-        ngDialog.open({ template: 'popupTmpl.html' });
-    };
-});
