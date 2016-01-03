c830846a48c763a096da2cb88b9519a86efdca4f
Blenesi Attila <ablenesi@gmail.com>
2016-01-03 20:23:14 +0200

    Added Datamanager unit test

---
+++ VoiceConf/app/src/test/java/com/voiceconf/voiceconf/storage/nonpersistent/DataManagerTest.java
@@ -0,0 +1,54 @@
+package com.voiceconf.voiceconf.storage.nonpersistent;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Attila Blenesi on 03 Jan 2016
+ */
+public class DataManagerTest {
+
+    DataManager mDataManager;
+
+    @Before
+    public void setUp() throws Exception {
+        mDataManager = new DataManager();
+    }
+
+    @Test
+    public void testGetFriends() throws Exception {
+        assertEquals(1,1);
+    }
+
+    @Test
+    public void testSetConferences() throws Exception {
+        assertEquals(1,1);
+
+    }
+
+    @Test
+    public void testGetConferences() throws Exception {
+        assertEquals(1,1);
+
+    }
+
+    @Test
+    public void testGetUsers() throws Exception {
+        assertEquals(1,1);
+
+    }
+
+    @Test
+    public void testGetUsers1() throws Exception {
+        assertEquals(1,1);
+
+    }
+
+    @Test
+    public void testGetConference() throws Exception {
+        assertEquals(1,1);
+
+    }
+}
