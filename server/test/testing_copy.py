6079d5069a4c2f2f3fd327071a7010b434c673cf
Kádár Tamás-Csaba <triqla@gmail.com>
2016-01-04 20:50:44 +0200

    Fixing path bug

--- server/test/testing_copy.py
+++ server/test/testing_copy.py
@@ -6,6 +6,7 @@ import os
 # you must pre-configure putty/pscp to use pub key auth
 def remote_copy(server, user, src, dst, passw):
     path = os.getcwd()
+    path = "\ ".join(path.split(" "))
     print path
     p = Popen(
         '%(path)spscp -pw %(passw)s %(src)s %(user)s@%(server)s:%(dst)s' % locals(), 
