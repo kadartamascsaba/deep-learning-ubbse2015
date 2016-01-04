b8e4369e99cb818aed2a7b19c084bc53d42ab956
Kádár Tamás-Csaba <triqla@gmail.com>
2016-01-04 20:43:35 +0200

    Fixing indentation bug

--- server/test/testing_copy.py
+++ server/test/testing_copy.py
@@ -5,7 +5,7 @@ import os
 
 # you must pre-configure putty/pscp to use pub key auth
 def remote_copy(server, user, src, dst, passw):
-	path = os.getcwd()
+    path = os.getcwd()
     print path
     p = Popen(
         '%(path)spscp -pw %(passw)s %(src)s %(user)s@%(server)s:%(dst)s' % locals(), 
