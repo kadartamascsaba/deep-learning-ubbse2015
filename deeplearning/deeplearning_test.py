9a36b4ad48eafc45a05a2d63f6762b1c82cac024
Kádár Tamás-Csaba <triqla@gmail.com>
2016-01-05 16:11:05 +0200

    Changed test value

--- deeplearning/deeplearning_test.py
+++ deeplearning/deeplearning_test.py
@@ -11,7 +11,7 @@ class TestDeepLearning(unittest.TestCase):
 
     # spectrogram.stereo_to_mono is expected to return -1 if the number of dimensions of the input array is > 2
     def testStereoToMonoConverterReturnsSpecialValueInCaseOfWrongDimensions(self):
-        self.assertEqual(spectrogram.stereo_to_mono(numpy.array([[[]]])), 2)
+        self.assertEqual(spectrogram.stereo_to_mono(numpy.array([[[]]])), -1)
 
     # spectrogram.stft is expected to raise exception if the input is array
     def testStftRaisesExceptionInCaseOfEmptyInput(self):
